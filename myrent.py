24b027081e2bec1e5cf2728b35bb91471c8ba22c
Grzegorz Zając <gzajac11@gmail.com>
2020-11-24 03:00:17 +0100

    change in db drivername

--- myrent.py
+++ myrent.py
@@ -2,8 +2,6 @@ from myrent_app import create_app
 
 app = create_app()
 
-key = app.config['SECRET_KEY']
-
 @app.route('/')
 def index():
-    return f'flask-myrent-api, prefiks=/api/v1, author: Grzegorz Zając {key}'
+    return f'flask-myrent-api, prefiks=/api/v1, author: Grzegorz Zając'
