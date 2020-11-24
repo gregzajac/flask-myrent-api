b4bf25ffc4f4d0f835a30826a864b12d1cd1b10f
Grzegorz Zając <gzajac11@gmail.com>
2020-11-24 03:25:33 +0100

    db uri changes

--- wsgi.py
+++ wsgi.py
@@ -2,12 +2,11 @@ from myrent_app import create_app
 
 app = create_app('production')
 ver = app.config['VERSION']
-uri = app.config['SQLALCHEMY_DATABASE_URI']
 
 
 @app.route('/')
 def index():
-    return f'flask-myrent-api, prefiks=/api/{ver}, author: Grzegorz Zając, uri: {uri}'
+    return f'flask-myrent-api, prefiks=/api/{ver}, author: Grzegorz Zając'
 
 if __name__ == "__main__":
     app.run()
