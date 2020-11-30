56b5a84022b72d902b6f6143c05182b89bd06070
Grzegorz Zając <gzajac11@gmail.com>
2020-11-30 19:40:50 +0100

    change in prod config

--- config.py
+++ config.py
@@ -41,7 +41,8 @@ class ProductionConfig(Config):
     DB_HOST = os.environ.get('DB_HOST')
     DB_PORT = os.environ.get('DB_PORT')
     DB_NAME = os.environ.get('DB_NAME')
-    SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
+    # SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
 
 config = {
